6c713d4f7816f5dee5106a513bbd61381a7ef8d8
EstebanPdN <72305261+EstebanPdN@users.noreply.github.com>
2026-04-22 04:44:23 +0000

    Added remaining recursive methods and created test file

--- RecursionProblems.java
+++ RecursionProblems.java
@@ -15,3 +15,43 @@ public class RecursionProblems {
         return count8(n / 10);
     }
 }
+
+public static int countHi(String str) {
+    if (str.length() < 2) return 0;
+
+    if (str.substring(0, 2).equals("hi")) {
+        return 1 + countHi(str.substring(2));
+    }
+
+    return countHi(str.substring(1));
+}
+
+public static int countHi2(String str) {
+    if (str.length() < 2) return 0;
+
+    if (str.substring(0, 2).equals("hi") && str.charAt(0) != 'x') {
+        return 1 + countHi2(str.substring(2));
+    }
+
+    return countHi2(str.substring(1));
+}
+
+public static int strCount(String str, String sub) {
+    if (str.length() < sub.length()) return 0;
+
+    if (str.substring(0, sub.length()).equals(sub)) {
+        return 1 + strCount(str.substring(sub.length()), sub);
+    }
+
+    return strCount(str.substring(1), sub);
+}
+
+public static String stringClean(String str) {
+    if (str.length() < 2) return str;
+
+    if (str.charAt(0) == str.charAt(1)) {
+        return stringClean(str.substring(1));
+    }
+
+    return str.charAt(0) + stringClean(str.substring(1));
+}
